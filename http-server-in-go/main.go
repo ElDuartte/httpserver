1a417e0ce0b2c8096ff69f97dcb609788e1563c5
juan felipe <jf@duartte.co>
2024-09-12 14:00:35 +0200

    Adding a markdown where I can add comments and notes plus adding error handling

--- http-server-in-go/main.go
+++ http-server-in-go/main.go
@@ -8,19 +8,25 @@ import(
 	"os"
 )
 
-func getRoot(w http.ResponseWritter, r *http.Request) {
+func getRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Got / request\n")
 	io.WriteString(w, "This is my web\n")
 }
 
-func getHello(w http.ResponseWritter, r *http.Request){
+func getHello(w http.ResponseWriter, r *http.Request){
 	fmt.Printf("got /hello request\n")
 	io.WriteString(w, "Hello, http\n")
 }
 
 func main(){
 	http.HandleFunc("/", getRoot)
-	http.HandleFunc('/hello', getHello)
+	http.HandleFunc("/hello", getHello)
 
-	err := http.ListenAndServe(":6969", nil)
+	err := http.ListenAndServe("127.0.0.1:6969", nil)
+	if errors.Is(err, http.ErrServerClosed){
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
 }
---
+++ http-server-in-go/understanding.md
@@ -0,0 +1,4 @@
+# Here is the stuff I'm adding and understanding to the server
+
+The most similar thing that I can describe an http server is an "event listener" in JS
+Waits for a request then delivers according to the request
